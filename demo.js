07d74c8f42331a1d074ca38204ff8fdf45fcd5af
Aziz Khoury <bentael@gmail.com>
2013-02-26 19:18:11 -0500

    adding documentation, minor syntax fixes

--- demo.js
+++ demo.js
@@ -3,17 +3,16 @@
 var GinxParser = require('./lib/ginxparser');
 //construct a parser object, optionally you can pass in the Nginx access_log format. if you don't it will use the default
 var parser = new GinxParser();
-
 /**
- * FOR DEMO PURPOSES ONLY
- * I use the GinxParserTest to create 30 copy of log files in 
+ * ======README PLEASE====== FOR DEMO PURPOSES ONLY ==========
+ * I use the GinxParserTest to create N copies of log files in 
  *   ./test/tmplogs from either ./test/logs/nginx_prod-large.log or ./test/logs/nginx_prod-small.log 
  * you will usually have your own log files
- * and since I need to wait for these the files to be created before I actually start parsing, I pass the parsing code in a callback
+ * and since I need to wait for these the files to be copied before I actually start parsing, I pass the parsing code in a callback function
  */
  var GinxParserTest = require('./test/ginxparsertest')
  var parserTest = new GinxParserTest();
- parserTest.setupTest(30, 'large', function(){
+ parserTest.setupTest(10, 'small', function(){
    
    /**
     * parses all files in level 0 of a directory (NOT RECURSIVE)
@@ -30,7 +29,7 @@ var parser = new GinxParser();
          //console.log(row);
        },
      function(file){
-      console.log(file + " parsing complete");
+      console.log("[DEBUG] " + file + " parsing completed in ParseDir()");
      }
    );
    /**
@@ -48,7 +47,8 @@ var parser = new GinxParser();
           //console.log(row);
         },
       function(file){
-       console.log(file + " parsing complete");
+       console.log("[DEBUG] " + file + " parsing completed in ParseFile()");
       }
     );
+    
  });
--- test/ginxparsertest.js
+++ test/ginxparsertest.js
@@ -1,47 +1,37 @@
  var fs = require('fs-extra'), path = require('path');
 
   /**
-   * GinxParser constructor
-   *
-   * @format specified in your nginx conf. If you haven't specified anything, it will use nginx's default
+   * GinxParserTest constructor
+   *   
    */
-  function GinxParserTest(format){
-    var default_format = '$remote_addr - $remote_user [$time_local] ' +
-                          '"$request" $status $body_bytes_sent ' +  
-                          '"$http_referer" "$http_user_agent"';
-    this.format = format || default_format;
-    this.delimeters = this.format.match(/[^\$\w+]+/g);
-    this.attrs = this.format.match(/\$\w+/g);
+  function GinxParserTest(){
     this.defaultCopies = 30;
     this.defaultOrgSmallLog = path.normalize('./test/logs/nginx_prod-small.log');
     this.defaultOrgLargeLog = path.normalize('./test/logs/nginx_prod-large.log');
     this.defaultTmpLogsDir = path.normalize('./test/tmplogs');
     this.LARGE = 'large';
-    this.small = 'small';
+    this.SMALL = 'small';
   }
 
-//TESTING HELPER FUNCTIONS
+//TEST HELPER FUNCTIONS
 
 GinxParserTest.prototype.setupTest = function(count,logSize, callback){
   var dir = './test/tmplogs';
   var that = this;
   fs.remove(dir, function(){
     fs.mkdirs(dir, function(){
-     //testing a random dir in the log dir, THIS WILL BLOCK just to test
-     fs.mkdirSync(path.join(dir,'randomFolder')); 
      that.copyLogFiles(count, logSize, callback);
     });
   });
 };
-
 GinxParserTest.prototype.copyLogFiles = function(n, size, callback){
+  console.log("[TEST] copying of " + n + " " + size + " log files started");
   if (size === this.SMALL){
     this.copySmallLogs(n, callback);
   } else if (size === this.LARGE){
     this.copyLargeLogs(n, callback);
   }
 };
-
 GinxParserTest.prototype.copyFileMultipleToTmpLogs = function(nbCopies, file, callback){
   var trgFile, srcFile = file, that = this;
   if (nbCopies > 0 ){  
@@ -52,6 +42,7 @@ GinxParserTest.prototype.copyFileMultipleToTmpLogs = function(nbCopies, file, ca
     });
   }
   else {
+    console.log("[TEST] All test log files copied.")
     callback(true)
   }
 }
